a45e999757ff0da2f26277da04bdb6e627541fbd
Rooshy <ru_sha_nia@yahoo.ca>
2016-03-15 15:30:34 -0500

    initial commit

---
+++ JavaScript- Methods.js
@@ -0,0 +1,20 @@
+
+var helloMessage = "Hello World";
+console.log(helloMessage);
+
+var differentVariable = "different string";
+console.log(differentVariable);
+
+var someNumber = 7;
+console.log("Some number is " + someNumber);
+
+var age = 78;
+console.log("My grandmother is " + age + " years old!");
+
+var myFavBooks = [
+  "Ruby on Rails",
+  "Eloquent Ruby",
+  "Eloquent Javascript",
+  "Hello Ruby"
+];
+console.log(myFavBooks);
